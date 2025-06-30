6367feaed577bb184a880a891d6e04b8cc4c52b1
Sudarsh25 <144419617+Sudarsh25@users.noreply.github.com>
2025-06-30 11:28:55 +0530

    Add files via upload

---
+++ Udp_chat_app/udp_client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[]) {
+    int sockfd;
+    char buffer[1024];
+    struct sockaddr_in servaddr;
+
+    if (argc != 3) {
+        printf("Usage: %s <server_ip> <port>\n", argv[0]);
+        return -1;
+    }
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return -1;
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+    while (1) {
+        printf("You: ");
+        fgets(buffer, sizeof(buffer), stdin);
+
+        if (strncmp(buffer, "exit", 4) == 0) break;
+
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+        int n = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
+        buffer[n] = '\0';
+        printf("Server: %s\n", buffer);
+    }
+
+    return 0;
+}
+
